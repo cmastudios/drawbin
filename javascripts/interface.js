61b0c4541e5eec51e7a348af93850038fc452ff7
Mark Castle <markacastle@Marks-MacBook-Pro.local>
2013-10-09 22:41:40 -0500

    Something fixed

--- javascripts/interface.js
+++ javascripts/interface.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function() {
-<<<<<<< HEAD
   //var drawingWidth = parseInt(prompt('Drawing width in pixels?'));
   //var drawingHeight = parseInt(prompt('Drawing height in pixels?'));
   var drawingWidth = 750;
@@ -39,31 +38,4 @@ function drawSample() {
     x: 75, y:75,
     radius: 50
   })
-=======
-  drawingWidth = parseInt(prompt('Drawing width in pixels?'));
-  drawingHeight = parseInt(prompt('Drawing height in pixels?'));
-  jQuery('#drawing').width(drawingWidth);
-  jQuery('#drawing').height(drawingHeight);
-  jQuery('.container').css('width', drawingWidth + .1 * jQuery(window).width());
-});
-
-jQuery(window).on('resize', function(){
-  jQuery('.container').css('width', drawingWidth + .1 * jQuery(window).width());
-});
-
-function menu() {
-  alert('Wow, great job! You done diggity clicked on something!');
-}
-
-function brushes() {
-  alert('Wow, great job! You done diggity clicked on something!');
-}
-
-function upload() {
-  alert('Wow, great job! You done diggity clicked on something!');
-}
-
-function download() {
-  alert('Wow, great job! You done diggity clicked on something!');
->>>>>>> ee0998c8a13bcf648148787603817666323282b3
 }
